ccafd34107105957a4e288258a8b5c7727b6f91e
Cloud Xu <cloudxunuo@gmail.com>
2011-03-06 11:31:14 +0800

    level1-7

---
+++ Demeter/Source/Game1.cs
@@ -0,0 +1,325 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using System.Collections;
+
+namespace Demeter
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager graphics;
+        public GraphicsDeviceManager Graphics
+        {
+            get { return graphics; }
+        }
+
+        private SpriteBatch spriteBatch;
+        public SpriteBatch SpriteBatch
+        {
+            get { return spriteBatch; }
+        }
+
+        private Level level;
+        public Level Level
+        {
+            get { return level; }
+        }
+
+        BindingPoint bindingPoint;
+        public BindingPoint BindingPoint
+        {
+            get { return bindingPoint; }
+            set { bindingPoint = value; }
+        }
+
+        int width = 1366;
+        public int Width
+        {
+            get { return this.width; }
+        }
+
+        int height = 768;
+        public int Height
+        {
+            get { return this.height; }
+        }
+
+        public int HalfWidth
+        {
+            get { return this.width / 2; }
+        }
+
+        public int HalfHeight
+        {
+            get { return this.height / 2; }
+        }
+
+        public SpriteFont font;
+
+        private int dieTime;
+        string current_levelFileName;
+        public string Current_levelFileName
+        {
+            get { return current_levelFileName; }
+        }
+        Vector2 diePosition;
+        public Vector2 DiePosition
+        {
+            get { return diePosition; }
+        }
+
+        #region menu relative
+        bool gotoMenu = false;
+        bool wasUpKeydown = false;
+        bool wasDownKeydown = false;
+
+        int currentSelection = 1;
+        const int maxItemsCount = 4;
+
+        Vector2[] menuPos = new Vector2[maxItemsCount];
+        #endregion
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+
+            graphics.PreferredBackBufferWidth = width;
+            graphics.PreferredBackBufferHeight = height;
+
+            /*if (!graphics.IsFullScreen)
+                graphics.ToggleFullScreen();*/
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            for (int i = 0; i < maxItemsCount; i++)
+            {
+                menuPos[i] = new Vector2(200 , i*50 + 200);
+            }
+                // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            font = Content.Load<SpriteFont>("font/Hud");
+
+            level = new Level(this);
+            level.Load("TotalLevel.xml");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                gotoMenu = true;
+            }
+
+            if (!gotoMenu)
+            {
+                level.Update(gameTime);
+
+                if (level.Player.IsLeaving)
+                {
+                    string commingLevel = Level.Player.ComingLevel;
+                    if (commingLevel != "null")
+                    {
+                        level = new Level(this);
+                        level.Load(commingLevel);
+                    }
+                    level.Player.IsLeaving = false;
+                }
+
+                if (!level.Player.IsAlive)
+                {
+                    dieTime += gameTime.ElapsedGameTime.Milliseconds;
+                    if (dieTime > level.Player.DieTime)
+                    {
+                        current_levelFileName = level.LevelFileName;
+                        diePosition = new Vector2(level.Player.Position.X,
+                            level.Player.Position.Y);
+                        level = new Level(this);
+                        level.Load(current_levelFileName);
+                        dieTime = 0;
+                    }
+                }
+            }
+            else
+            {
+                MenuUpdate();
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Gray);
+
+            // TODO: Add your drawing code here
+            SpriteBatch.Begin(SpriteBlendMode.AlphaBlend, SpriteSortMode.FrontToBack, SaveStateMode.SaveState);
+            if (!gotoMenu)
+            {
+                currentSelection = 1;
+                level.Draw(gameTime);
+            }
+            else
+            {
+                MenuDraw();
+            }
+            SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public void MenuUpdate()
+        {
+            // select options
+            KeyboardState keystate = Keyboard.GetState();
+            if (keystate.IsKeyDown(Keys.Down))
+            {
+                if (!wasDownKeydown)
+                {
+                    currentSelection++;
+                    if (Level.LevelFileName.IndexOf("-") < 0 && currentSelection == 2)
+                    {
+                        currentSelection++;
+                    }
+                    if (currentSelection > maxItemsCount)
+                    {
+                        currentSelection = 1;
+                    }
+                }
+                wasDownKeydown = true;
+            }
+            else
+            {
+                wasDownKeydown = false;
+            }
+
+            if (keystate.IsKeyDown(Keys.Up))
+            {
+                if (!wasUpKeydown)
+                {
+                    currentSelection--;
+                    if (Level.LevelFileName.IndexOf("-") < 0 && currentSelection == 2)
+                    {
+                        currentSelection--;
+                    }
+                    if (currentSelection < 1)
+                    {
+                        currentSelection = maxItemsCount;
+                    }
+                }
+                wasUpKeydown = true;
+            }
+            else
+            {
+                wasUpKeydown = false;
+            }
+
+            // execute the selectioon
+            if (currentSelection == 1 && keystate.IsKeyDown(Keys.Enter))
+            {
+                gotoMenu = false;
+            }
+            else if (currentSelection == 2 && keystate.IsKeyDown(Keys.Enter))
+            {
+                if (Level.LevelFileName.IndexOf("-") > 0)
+                {
+                    bindingPoint = null;
+                    gotoMenu = false;
+                    String general_level = Level.LevelFileName.Substring(0, 6);
+                    general_level += ".xml";
+                    level = new Level(this);
+                    level.Load(general_level);
+                }
+            }
+            else if (currentSelection == 3 && keystate.IsKeyDown(Keys.Enter))
+            {
+                bindingPoint = null;
+                Level.TreasureMgr.ResetLevel();
+                gotoMenu = false;
+                LoadContent();
+            }
+            else if (currentSelection == 4 && keystate.IsKeyDown(Keys.Enter))
+            {
+                this.Exit();
+            }
+        }
+
+        public void MenuDraw()
+        {
+            if (currentSelection != 1)
+                spriteBatch.DrawString(font, "Resume Game", menuPos[0], Color.White);
+            if (currentSelection != 2)
+                spriteBatch.DrawString(font, "Leave Current Level", menuPos[1], Color.White);
+            if (currentSelection != 3)
+                spriteBatch.DrawString(font, "Restart Game", menuPos[2], Color.White);
+            if (currentSelection != 4)
+                spriteBatch.DrawString(font, "Quit Game", menuPos[3], Color.White);
+
+            switch (currentSelection)
+            {
+                case 1:
+                    spriteBatch.DrawString(font, "Resume Game", menuPos[0], Color.Red);
+                    break;
+                case 2:
+                    spriteBatch.DrawString(font, "Leave Current Level", menuPos[1], Color.Red);
+                    break;
+                case 3:
+                    spriteBatch.DrawString(font, "Restart Game", menuPos[2], Color.Red);
+                    break;
+                case 4:
+                    spriteBatch.DrawString(font, "Quit Game", menuPos[3], Color.Red);
+                    break;
+            }
+        }
+    }
+}
---
+++ Demeter/Source/Helper/BindingPoint.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Demeter
+{
+    public class BindingPoint
+    {
+        List<Vector2> totalPoints;
+        List<String> totalLevels;
+
+        List<Vector2> points;
+        List<String> levels;
+        Vector2 binding;
+
+        public BindingPoint()
+        {
+            points = new List<Vector2>();
+            levels = new List<string>();
+            totalPoints = new List<Vector2>();
+            totalLevels = new List<string>();
+        }
+
+        public void Add(Vector2 bindingPoint,String level)
+        {
+            totalPoints.Add(bindingPoint);
+            totalLevels.Add(level);
+        }
+
+        public void PassBindingPoint(string currentLevel, Vector2 playerPos)
+        {
+            for (int i = 0; i < totalLevels.Count; i++)
+            {
+                if (totalLevels[i] == currentLevel)
+                {
+                    if (playerPos.X >= totalPoints[i].X)
+                    {
+                        points.Add(totalPoints[i]);
+                        levels.Add(totalLevels[i]);
+                    }
+                }
+            }
+        }
+
+        public Vector2 JudgeBindingPoint(string currentLevel , Vector2 playerPos)
+        {
+            int minDistance = 10000;
+
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levels[i] == currentLevel)
+                {
+                    if (Math.Abs(points[i].X - playerPos.X) < minDistance)
+                    {
+                        minDistance = (int)Math.Abs(points[i].X - playerPos.X);
+                        binding = points[i];
+                    }
+                }
+            }
+            return binding;
+        }
+    }
+}
